2b9e7e41562d1702331556fdade82a84ac53e8a0
Tim van Dijen <tvdijen@gmail.com>
2019-09-13 15:38:16 +0200

    Stop using string issuers

--- modules/saml/lib/IdP/SAML2.php
+++ modules/saml/lib/IdP/SAML2.php
@@ -368,13 +368,8 @@ class SAML2
                 throw new Error\BadRequest(
                     'Received message on authentication request endpoint without issuer.'
                 );
-            } else {
-                $spEntityId = $issuer->getValue();
-                if ($spEntityId === null) {
-                    /* Without an issuer we have no way to respond to the message. */
-                    throw new Error\BadRequest('Received message on logout endpoint without issuer.');
-                }
             }
+            $spEntityId = $issuer->getValue();
             $spMetadata = $metadata->getMetaDataConfig($spEntityId, 'saml20-sp-remote');
 
             \SimpleSAML\Module\saml\Message::validateMessage($spMetadata, $idpMetadata, $request);
